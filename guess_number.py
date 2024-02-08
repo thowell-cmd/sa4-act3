8376458abc665c2cdea4a87cc3b8ae7e8e88696b
Tyler Howell <tyler.howell1202@gmail.com>
2024-02-07 20:06:12 -0600

    Modified to limit the number of guesses a user has

--- guess_number.py
+++ guess_number.py
@@ -1,8 +1,9 @@
 number = 10
 print("I'm thinking of a number...")
+guess_count = 5
 
-while True:
-    guess = input("What number am I thinking of? ('q' to quit) ")
+while guess_count > 0:
+    guess = input(f"What number am I thinking of? {guess_count} guesses left ('q' to quit) ")
 
     if guess == 'q':
         print(f"The number was {number}.")
@@ -11,4 +12,9 @@ while True:
         print("Congratulations! You guessed the right number.")
         break
     else:
-        print(f"Sorry! That's not quite right. Try.")
+        guess_count -= 1
+        if guess_count > 0:
+            print(f"Sorry! That's not quite right. Try again...")
+        else:
+            print(f'No more guesses left! The number I was thinking of was {number}')
+            break
